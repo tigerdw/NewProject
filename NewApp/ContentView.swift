524648ab4c81d663e8a3ca681b7a367142136ab1
Brian <tiger796@naver.com>
2020-04-19 12:07:59 -0400

    initial commit with initial file

---
+++ NewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewApp
+//
+//  Created by brian Chung on 19/4/2020.
+//  Copyright © 2020 brian Chung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
